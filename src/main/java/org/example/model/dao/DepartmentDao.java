e39cb0d5956d53b9361cc368782021b7a94f0b1d
kellyxwz <kellyramosdeoliveira25@gmail.com>
2026-04-11 21:54:50 -0300

    criando as interfaces

---
+++ src/main/java/org/example/model/dao/DepartmentDao.java
@@ -0,0 +1,15 @@
+package org.example.model.dao;
+
+import org.example.model.Department;
+
+import java.util.List;
+
+public interface DepartmentDao {
+
+    void insert (Department obj);
+    void update (Department obj);
+    void deleteById (Integer id);
+    Department findById (Integer id);
+    List<Department> findAll ();
+
+}
---
+++ src/main/java/org/example/model/dao/SellerDao.java
@@ -0,0 +1,16 @@
+package org.example.model.dao;
+
+import org.example.model.Department;
+import org.example.model.Seller;
+
+import java.util.List;
+
+public interface SellerDao {
+
+    void insert (Seller obj);
+    void update (Seller obj);
+    void deleteById (Integer id);
+    Seller findById (Integer id);
+    List<Seller> findAll ();
+
+}
